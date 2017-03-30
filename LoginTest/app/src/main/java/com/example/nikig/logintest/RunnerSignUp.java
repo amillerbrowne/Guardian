625b9d3c450ac61ed97dfebf0a4d9d941485a47e
Andrew Hoang <ahoang18@bu.edu>
2017-03-30 16:52:17 -0400

    Save display name on signup

--- LoginTest/app/src/main/java/com/example/nikig/logintest/RunnerSignUp.java
+++ LoginTest/app/src/main/java/com/example/nikig/logintest/RunnerSignUp.java
@@ -14,11 +14,9 @@ import android.widget.Toast;
 
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
-import com.google.firebase.database.DataSnapshot;
-import com.google.firebase.database.DatabaseError;
+import com.google.firebase.auth.UserProfileChangeRequest;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
-import com.google.firebase.database.ValueEventListener;
 
 
 public class RunnerSignUp extends AppCompatActivity {
@@ -31,7 +29,7 @@ public class RunnerSignUp extends AppCompatActivity {
     private Toast toast = null;
 
     // [START declare_database_ref]
-    private DatabaseReference mDatabase;
+    private DatabaseReference mDatabaseUsers;
     private FirebaseAuth Auth;
     // [END declare_database_ref]
 
@@ -41,11 +39,8 @@ public class RunnerSignUp extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.runner_signup);
 
-
-        //initialize FirebaseDatavase
-        mDatabase = FirebaseDatabase.getInstance().getReference();
+        mDatabaseUsers = FirebaseDatabase.getInstance().getReference("users");
         Auth = FirebaseAuth.getInstance();
-
 
         wel = (TextView) findViewById(R.id.welcome);
         first = (EditText) findViewById(R.id.rfirst);
@@ -64,52 +59,27 @@ public class RunnerSignUp extends AppCompatActivity {
 
             @Override
             public void onClick(View v) {
-               mDatabase = FirebaseDatabase.getInstance().getReference("users");
 
-// Creating new user node, which returns the unique key value
-// new user node would be /users/$userid/
-                String userId = Auth.getCurrentUser().getUid();
-
-// creating user object
-
-
-// pushing user to 'users' node using the userId
-
-//                mDatabase = FirebaseDatabase.getInstance().getReference("users");
                 UserInformation userInfo = new UserInformation(first.getText().toString().trim(),
                        last.getText().toString().trim(),dob.getText().toString().trim(), age.getText().toString().trim());
-//
-//
-//
-//// pushing user to 'users' node using the userId
-               mDatabase.child(userId).setValue(userInfo);
 
-               final FirebaseUser user = Auth.getCurrentUser();
-//
-                mDatabase.child(user.getUid()).setValue(userInfo);
+                final FirebaseUser user = Auth.getCurrentUser();
+                mDatabaseUsers.child(user.getUid()).setValue(userInfo);
+
+                // Now update the information in the User's Auth profile as well
+                UserProfileChangeRequest update = new UserProfileChangeRequest.Builder()
+                        .setDisplayName(first.getText().toString().trim() + last.getText().toString().trim())
+                        .build();
+                user.updateProfile(update);
 
                 toast.makeText(getApplicationContext(), "Posting Data...", Toast.LENGTH_SHORT).show();
+                wel.setText("Welcome "+ userInfo.getN());
 
-                //Value event listener for realtime data update
+                startActivity(new Intent(RunnerSignUp.this, Home.class));
+                finish();
+            }
 
-
-//                            String string = "Name: "+person.getName()+"\nAddress: "+person.getAddress()+"\n\n";
-
-//                            //Displaying it on textview
-
-                           wel.setText("Welcome "+ userInfo.getN());
-
-                        }
-
-
-
-
-    //startActivity(new Intent(RunnerSignUp.this, Home.class));
-                //finish();
-
-                //finish();
-
-            });
-        }
+        });
     }
+}
 
