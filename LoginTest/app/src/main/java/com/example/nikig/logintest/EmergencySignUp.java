7802f05746e46db849c3aa1c83284eadb5111565
nikig04 <nikig04@bu.edu>
2017-03-30 20:10:04 -0400

    emergency contact

--- LoginTest/app/src/main/java/com/example/nikig/logintest/EmergencySignUp.java
+++ LoginTest/app/src/main/java/com/example/nikig/logintest/EmergencySignUp.java
@@ -2,12 +2,40 @@ package com.example.nikig.logintest;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.database.DatabaseReference;
 
 public class EmergencySignUp extends AppCompatActivity {
+    private EditText eName;
+    private EditText eLast;
+    private EditText phone;
+    private EditText relation;
+    private Button Submit;
+
+    private DatabaseReference emergency;
+    private FirebaseAuth Auth;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.emergency_signup);
-    }
-}
+
+        eName = (EditText) findViewById(R.id.efirst);
+         eLast = (EditText) findViewById(R.id.elast);
+         phone = (EditText) findViewById(R.id.primary);
+       relation = (EditText) findViewById(R.id.relation);
+         Submit = (Button) findViewById(R.id.emergencysubmit);
+
+
+     Submit.setOnClickListener(new View.OnClickListener() {
+
+         @Override
+         public void onClick(View v) {
+
+         }
+     }
